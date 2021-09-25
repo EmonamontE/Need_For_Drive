79e19ebc7821ab7779b26c3a39b228f93f17be09
emonamonte <racoonsmr@gmail.com>
2021-09-25 20:08:52 +0400

    Final changes

--- src/components/mainPage/Sidebar/Sidebar.jsx
+++ src/components/mainPage/Sidebar/Sidebar.jsx
@@ -1,20 +1,18 @@
 import styles from "./sidebar.module.css";
 
-const Sidebar = ({ menuOpenHandler, languageHandler, language }) => {
-  return(
-    <nav className={ styles.sidebar }>
-      <button
-        className={ styles.sandwich_btn }
-        onClick={ menuOpenHandler }
-      />
-      <button
-        className={ styles.language_btn }
-        onClick={ languageHandler }
-      >
-        { language }
-      </button>
-    </nav>
-  );
-}
+const Sidebar = ({ menuOpenHandler, languageHandler, language }) => (
+  <nav className={ styles.sidebar }>
+    <button
+      className={ styles.sandwich_btn }
+      onClick={ menuOpenHandler }
+    />
+    <button
+      className={ styles.language_btn }
+      onClick={ languageHandler }
+    >
+      { language }
+    </button>
+  </nav>
+);
 
 export default Sidebar;
